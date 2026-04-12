0ad3551e76792e3d30e4b28f595fcc7974ad4759
DDD <dayeon3721@gmail.com>
2026-04-13 00:25:25 +0900

    아빠 만들기
    + 땅파는 로직 개선

---
+++ Source/Dino/Public/VoxelExposureLibrary.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "VoxelExposureLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DINO_API UVoxelExposureLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
