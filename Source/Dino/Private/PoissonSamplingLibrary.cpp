f3ecab257e1742d1c8ded26b76fa698da0e75305
DDD <dayeon3721@gmail.com>
2026-05-03 20:31:13 +0900

    땅 속 오브젝트 배치 로직 작성
    1. Poisson Disk Sampling과 유사한 방식으로 최소 거리 기반한 위치 분포 반환하는 c++클래스 추가 -> 블루프린트 노드 GeneratePoissonLikePoints로 사용 가능
    
    2. 배치할 아이템 결정 로직 추가. (블루프린트로 작성함)
    
    * 위의 내용 전부 BP_ItemSpawner에서 담당

---
+++ Source/Dino/Private/PoissonSamplingLibrary.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PoissonSamplingLibrary.h"
+
+PoissonSamplingLibrary::PoissonSamplingLibrary()
+{
+}
+
+PoissonSamplingLibrary::~PoissonSamplingLibrary()
+{
+}
---
+++ Source/Dino/Public/PointDistributionLibrary.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "PointDistributionLibrary.generated.h"
+
+UCLASS()
+class DINO_API UPointDistributionLibrary : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+
+    UFUNCTION(BlueprintCallable, Category = "ProcGen")
+    static TArray<FVector> GeneratePoissonLikePoints(
+        FVector2D Min,
+        FVector2D Max,
+        float MinDistance,
+        int32 TargetCount,
+        int32 MaxAttempts,
+        float Z,
+        int32 Seed
+    );
+};
