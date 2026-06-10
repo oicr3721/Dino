bb883af294400f0b54f6655f10a704c8eb765d60
DDD <dayeon3721@gmail.com>
2026-06-10 17:38:39 +0900

    게임 설명 추가

--- Source/Dino/Dino.Build.cs
+++ Source/Dino/Dino.Build.cs
@@ -8,7 +8,7 @@ public class Dino : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Kismet" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
